4896a149c1cb6103e93b1f60d257f39ead2faae3
LennyMaxMine <lennysebel@outlook.de>
2023-07-01 11:23:44 +0200

    Second Update = Projekt Cancelled

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#The Start
+# The Start
+import selenium
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome
