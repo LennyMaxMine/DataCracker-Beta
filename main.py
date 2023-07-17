17bf4f2eba688adb9f1437ad425662bd67e90b6e
LennyMaxMine <lennysebel@outlook.de>
2023-07-17 13:18:57 +0200

    Changed to Wifi read

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-# The Start
-import selenium
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import subprocess
 
-driver = webdriver.Chrome
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode(
+    'utf-8', errors='backslashreplace').split('\n')
+profiles = [i.split(':')[1][1:-1] for i in data if 'All User Profile' in i]
+
+for profile in profiles:
+    try:
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode(
+            'utf-8', errors='backslashreplace').split('\n')
+        passwords = [b.split(':')[1][1:-1]
+                     for b in results if 'Key Content' in b]
+        password = passwords[0] if passwords else ''
+        print("{:<30}|  {:<}".format(profile, password))
+    except subprocess.CalledProcessError:
+        print("{:<30}|  {:<}".format(profile, 'ENCODING ERROR'))
+
+input('')
