07da377aa9a971ed20bc829b01889dd19aed6313
LennyMaxMine <lennysebel@outlook.de>
2023-07-17 13:22:07 +0200

    Upgrade

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import subprocess
 
-print("Started")
+try:
+    print("Started")
 
-data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode(
-    'utf-8', errors='backslashreplace').split('\n')
-profiles = [i.split(':')[1][1:-1] for i in data if 'All User Profile' in i]
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode(
+        'utf-8', errors='backslashreplace').split('\n')
+    profiles = [i.split(':')[1][1:-1] for i in data if 'All User Profile' in i]
 
-print("Check One")
+    print("Check One")
 
-for profile in profiles:
-    try:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode(
-            'utf-8', errors='backslashreplace').split('\n')
-        passwords = [b.split(':')[1][1:-1]
-                     for b in results if 'Key Content' in b]
-        password = passwords[0] if passwords else ''
-        print("{:<30}|  {:<}".format(profile, password))
-    except subprocess.CalledProcessError:
-        print("{:<30}|  {:<}".format(profile, 'ENCODING ERROR'))
+    for profile in profiles:
+        try:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode(
+                'utf-8', errors='backslashreplace').split('\n')
+            passwords = [b.split(':')[1][1:-1]
+                         for b in results if 'Key Content' in b]
+            password = passwords[0] if passwords else ''
+            print("{:<30}|  {:<}".format(profile, password))
+        except subprocess.CalledProcessError:
+            print("{:<30}|  {:<}".format(profile, 'ENCODING ERROR'))
+        except IndexError:
+            print("{:<30}|  {:<}".format(profile, 'No Password Found'))
 
-print("Check Two")
+    print("Check Two")
+except Exception as e:
+    print("An error occurred:", str(e))
